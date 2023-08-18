4bc684cda139bc02c8505c1e670bac920dfab733
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-08-18 12:17:59 -0700

    fixing betty style

--- utils.c
+++ utils.c
@@ -1,23 +1,34 @@
 #include "main.h"
-int len (const char *str)
+/**
+ *len- caluc the len of a string
+ *@str: string
+ *Return: len of the string
+*/
+int len(const char *str)
 {
-    int x;
-    
-    x = 0;
-    while(str[x] != '\0')
-    {
-        x++;
-    }
-    return(x);
+	int x;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
 }
+/**
+ *is_digit- checks if the char is a digit
+ *@x: is the char
+ *Return: 0 if digit, 1 otherwise
+ *
+ */
 int is_digit(char x)
 {
-    if (x >= 48 && x <= 57)
-    {
-        return (0);
-    }
-    else
-    {
-        return (1);
-    }
+	if (x >= 48 && x <= 57)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
